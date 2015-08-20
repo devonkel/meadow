e13eefb278cab7b561910c45930d230a0a73d789
Devon Kelly <devon.kelly3@gmail.com>
2015-08-20 11:54:38 -0500

    End of Chapter 6

--- meadowlark.js
+++ meadowlark.js
@@ -19,6 +19,14 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.get('/headers', function(req,res){
+	res.set('Content-Type','text/plain');
+	var s='';
+	for(var name in req.headers)
+		s += name + ': ' + req.headers[name] + '\n';
+		res.send(s);
+});
+
 app.get('/tours/hood-river', function(req, res){
 	res.render('tours/hood-river');
 });
